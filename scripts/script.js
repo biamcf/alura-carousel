9ce3d63885cac8dd6e6a4d319d0ffda0d3aecc38
biamcf <abmoreira60@gmail.com>
2022-05-08 20:01:19 -0300

    First commit / Adição de tudo o que foi feito desde o lançamento da case.

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+let itens = document.querySelectorAll(".carousel-item");
+let max = itens.length / 4;
+let clicks = 0;
+
+if (max % 4 !== 0)
+	max = Math.floor(max) + 1;
+else
+	max = Math.floor(max);
+
+function moveForward() {
+	clicks++;
+	
+		for(let i = 0; i < itens.length; i++)
+			if (clicks > max) {
+				clicks = max;
+				break;
+			}
+			else if (clicks >= 0 && clicks <= max) {
+		    	itens[i].style.left = (-740 * clicks) + "px";
+		    	console.log("Avançou para " + i);		
+			}
+}
+
+function moveBack() {
+	clicks--; 
+
+		for(let i = 0; i < itens.length; i++)
+			if (clicks < 0) {
+				l = 0;
+				break;
+			}
+			else if (clicks >= 0 && clicks <= max) {
+				itens[i].style.left = (-740 * clicks) + "px";
+				console.log("Recuou para " + i);
+			}
+}
+
